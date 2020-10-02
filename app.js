938d42b65cd98b03c408999c81de948f80cd5f7e
Lindsey Smith <linds@lindseys-mbp.lan>
2020-10-02 16:39:14 -0600

    added initial prompt switch case

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
-const inquirer = require ('inquirer');
+const inquirer = require('inquirer');
 const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'greatBay_DB'
+});
+connection.connect(err => {
+    if(err) throw err;
+    start();
+});
+
+const start = () => {
+  inquirer.prompt({
+    name: "initial",
+    message: "What would you like to do?",
+    type: "list",
+    choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Remove employee", "Update employee role", "Update employee manager"]
+  }).then(answer => {
+    switch(answer.initial){
+      case "View all employees":
+        allEmployees();
+        break;
+      case "View all employees by department":
+        employeesByDep();
+        break;
+      case "View all employees by manager":
+        employeesByMan();
+        break;
+      case "Add employee":
+        addEmployee();
+        break;
+      case "Remove employee":
+        removeEmployee();
+        break;
+      case "Update employee role":
+        updateEmployeeRole();
+        break;
+      case "Update employee manager":
+        updateEmployeeManager();
+        break;
+    }
+  })
+}
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'password',
-    database : 'employeeTracker_DB'
+    database : 'employeeTracker_db'
 });
 connection.connect(err => {
     if(err) throw err;
-    start();
+
 });
