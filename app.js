a4d95fc2210b3dc172588871f7a290b6cc5a8c3e
Lindsey Smith <linds@lindseys-mbp.lan>
2020-10-07 14:44:07 -0600

    most of delete working

--- app.js
+++ app.js
@@ -30,11 +30,22 @@ const employeeRoleChoice = () => {
   connection.query(query, (err, res) => {
     if (err) throw err;
     res.forEach(title => array.push(title.title))
-
   })
   return array;
-  
 };
+
+//function for grabbing all employee names 
+
+const employeeList = () => {
+  let nameArray = [];
+  const query = "SELECT first_name, last_name FROM employee";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    res.forEach(name => nameArray.push(name.first_name + " " + name.last_name))
+  });
+  return nameArray;
+};
+
 
 
 const employeesByDep = () =>{
@@ -59,7 +70,7 @@ const addEmployee = () => {
     },
     {
       name:"employeeRole",
-      pageSize: 10,
+      pageSize: 15,
       type: "list",
       message: "What is the employees role?",
       choices: employeeRoleChoice()
@@ -83,28 +94,79 @@ const addEmployee = () => {
         if (err) throw err;
         start();
       });
-      
     })
-   
-   
-    // const setRole = "INSERT INTO employee_role (title) VALUES (?);";
-    // connection.query(setRole, answer.employeeRole, (err, res) => {
-    //   if (err) throw err;
-      
-    // });
-    // const setManager = "INSERT INTO manager (manager) VALUES (?);";
-    // connection.query(setManager, answer.employeeManager, (err, res) => {
-    //   if (err) throw err;
-     
-    //   start();
-    // })
-    
+  })
+  
+};
+const Employee = () => {
+  inquirer.prompt([
+    { 
+      name:"employeeRole",
+    pageSize: 15,
+    type: "list",
+    message: "What is the employees role?",
+    choices: employeeList()
+
+    },
+    {
+      name:"employeeRole",
+      pageSize: 15,
+      type: "list",
+      message: "What is the employees role?",
+      choices: employeeList()
+    },
+  ]).then(answer => {
+    // here we are taking the users selection for role and grabbing the role id
+    const getRoleId = `SELECT id FROM employee_role WHERE title = ${JSON.stringify(answer.employeeRole)}`
+    // then make a connection to the db and set the id to a variable
+    connection.query(getRoleId, (err,res) => {
+      if (err) throw err;
+      const roleId =res[0].id;
+      // then take the answers for all employee choices and insert into employee table
+      const createEmployee = `INSERT INTO employee (first_name, last_name, role_id) VALUES (? , ?, ?);`;
+      connection.query(createEmployee, [answer.firstName, answer.lastName, roleId],(err,res) => {
+        if (err) throw err;
+        start();
+      });
+    })
   })
   
 };
 
-const removeEmployee = () => {
+// const deleteEmployee = () => {
+//   inquirer.prompt([
+//     {
+//       name:"employeeRole",
+//       pageSize: 15,
+//       type: "list",
+//       message: "What is the employees role?",
+//       choices: employeeRoleChoice()
+//     },
+//   ])}
 
+const deleteEmployee = () => {
+  inquirer.prompt([
+    ///NEED TO FIGURE THIS OUT, why do I need a question in order for the second one to work
+    {
+      name: "confirmDelete",
+      type: "list",
+      message: "Are you sure you would like to delete an employee?",
+      choices: ['y','n']
+    },
+    {
+      type: 'list',
+      pageSize: 30,
+      message: 'What Employee would youlike to delete:',
+      name: "deleteName",
+      choices: employeeList()
+    },
+  ]).then(answer => {
+    const query = `DELETE FROM employee WHERE CONCAT(first_name, ' ', last_name) = ${JSON.stringify(answer.deleteName)}`
+    connection.query(query, (err,res) => {
+      if (err) throw err;
+      start();
+    })
+  });
 };
 
 const updateEmployeeRole = () => {
@@ -120,7 +182,7 @@ const start = () => {
     name: "initial",
     message: "What would you like to do?",
     type: "list",
-    choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Remove employee", "Update employee role", "Update employee manager"]
+    choices: ["View all employees", "View all employees by department", "View all employees by manager", "Add employee", "Delete employee", "Update employee role", "Update employee manager"]
   }).then(answer => {
     switch(answer.initial){
       case "View all employees":
@@ -135,8 +197,8 @@ const start = () => {
       case "Add employee":
         addEmployee();
         break;
-      case "Remove employee":
-        removeEmployee();
+      case "Delete employee":
+        deleteEmployee();
         break;
       case "Update employee role":
         updateEmployeeRole();
